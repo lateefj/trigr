586bd83b353ba956d3adbde05c33ad23b501de59
Lateef Jackson <lateef.jackson@gmail.com>
2020-03-03 12:36:41 -0800

    Test that pipeline concept is working

--- cmd/trigd/dsl.go
+++ cmd/trigd/dsl.go
@@ -16,7 +16,11 @@ import (
 
 // Configure the dsl
 func setupDsl(env []string, p *Project, t *trigr.Trigger, input *bytes.Buffer, output *bytes.Buffer, l *ext.TrigSL) {
-	l.SetGlobalVar("exec", func(cmd, directory string) string {
+
+	// Make all standard library available
+	l.LoadAllStdLibs()
+	lua.OpenBase(l.State)
+	l.SetGlobalVar("exec", luar.New(l.State, func(cmd, directory string) string {
 		split := strings.Split(cmd, " ")
 		c := split[0]
 		args := make([]string, 0)
@@ -32,11 +36,24 @@ func setupDsl(env []string, p *Project, t *trigr.Trigger, input *bytes.Buffer, o
 			t.Logs <- trigr.NewLog(err.Error())
 		}
 		return string(output)
-	})
+	}))
 
-	l.SetGlobalVar("publish_trigr", func(t *trigr.Trigger) {
-		p.Triggers <- t
-	})
+}
+
+// Start the pipeline rolling
+func callPipeline(pipelinePath string, l *ext.TrigSL, p *Project, t *trigr.Trigger) error {
+	f, err := os.Open(pipelinePath)
+	if err != nil {
+		return err
+	}
+	// Call the function
+	err = l.RunFunction(f, "handle_trigr", t, p.Triggers, luar.New(l.State, t))
+	if err != nil {
+		msgErr := fmt.Sprintf("Failed to run dsl file %s with error %s\n", pipelinePath, err)
+		t.Logs <- trigr.NewLog(msgErr)
+		return err
+	}
+	return nil
 }
 
 // handleTrigger mainly for local project configuration
@@ -46,37 +63,30 @@ func handleTrigger(env []string, p *Project, t *trigr.Trigger) {
 	if p.LocalSource != nil {
 		path := p.LocalSource.Path
 		// Handle streaming all trigr events to a single file
-		streamPath := fmt.Sprintf("%s/.trigr/stream.lua", path)
-		if _, err := os.Stat(streamPath); err == nil {
-			println("Stream file exists sending all trigr event to it")
-			l := ext.NewTrigSL(in, out, "./lsl/lua")
+		pipelinePath := fmt.Sprintf("%s/.trigr/pipeline.lua", path)
+		if _, err := os.Stat(pipelinePath); err == nil {
+			l := ext.NewTrigSL(in, out)
 			setupDsl(env, p, t, in, out, l)
-			/*err := l.DoFile(streamPath)
+			/*err := l.DoFile(pipelinePath)
 			if err != nil {
 				msgErr := fmt.Sprintf("Failed to run dsl file %s with error %s\n", streamPath, err)
 				t.Logs <- trigr.NewLog(msgErr)
 				log.Print(msgErr)
 			}*/
-			if err := l.State.CallByParam(lua.P{
-				Fn: l.State.GetGlobal("run_func_with_env"),
-				//Fn:      l.State.GetGlobal("handle_trigr"),
-				NRet:    1,
-				Protect: true,
-				//}, luar.New(l.State, t)); err != nil {
-			}, lua.LString(streamPath), lua.LString("handle_trigr"), luar.New(l.State, t)); err != nil {
-				msgErr := fmt.Sprintf("Failed to run dsl file %s with error %s\n", streamPath, err)
-				t.Logs <- trigr.NewLog(msgErr)
-				log.Print(msgErr)
+			callErr := callPipeline(pipelinePath, l, p, t)
+			if callErr != nil {
+				log.Printf("ERROR: %s\n", callErr)
+
 			}
 		}
 
 		// Handle specific types of trigr events to a file
 		luaPath := fmt.Sprintf("%s/.trigr/%s.lua", path, t.Type)
 		if _, err := os.Stat(luaPath); err == nil {
-			l := ext.NewTrigSL(in, out, "./lsl/lua")
+			l := ext.NewTrigSL(in, out)
 			setupDsl(env, p, t, in, out, l)
 			// Add the trig event to the context
-			l.SetGlobalVar("trig", t)
+			l.SetGlobalVar("trig", luar.New(l.State, t))
 			err = l.RunFile(luaPath, t, make(chan *trigr.Trigger))
 			if err != nil {
 				msgErr := fmt.Sprintf("Failed to run dsl file %s error %s\n", luaPath, err)
